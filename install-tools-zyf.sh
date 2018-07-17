50d3280bc3d0a26020bb30c74cef37bcbbf554a7
zyf <zhaoyafei0210@gmail.com>
2018-07-17 13:13:48 +0800

    update install-tools-zyf.sh, qshell/install_qshell.sh

--- install-tools-zyf.sh
+++ install-tools-zyf.sh
@@ -6,5 +6,12 @@ apt-get install -y -q zip
 apt-get install -y -q aria2
 apt-get install -y -q dos2unix
 
+## install qrsctl
 wget http://devtools.qiniu.com/linux/amd64/qrsctl-v3.2.20170501 -O qrsctl
 mv qrsctl /usr/bin
+
+## install qshell
+wget http://devtools.qiniu.com/qshell-v2.2.0.zip
+unzip qshell-v2.2.0.zip -d qshell-v2.2.0
+cp qshell-v2.2.0/qshell-linux-x64 /usr/bin/qshell
+rm -rf qshell-v2.2.0*
--- qshell/install_qshell.sh
+++ qshell/install_qshell.sh
@@ -3,3 +3,5 @@
 wget http://devtools.qiniu.com/qshell-v2.2.0.zip
 unzip qshell-v2.2.0.zip -d qshell-v2.2.0
 cp qshell-v2.2.0/qshell-linux-x64 /usr/bin/qshell
+
+rm -rf qshell-v2.2.0*
