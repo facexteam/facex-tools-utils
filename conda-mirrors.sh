effe392ebc5dca4e3ab1e952d13410483bf810dd
zyf <zhaoyafei0210@gmail.com>
2018-11-19 12:29:31 +0800

    add conda-mirrors.sh

---
+++ conda-mirrors.sh
@@ -0,0 +1,3 @@
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
