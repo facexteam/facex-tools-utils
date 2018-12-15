7e67a8b702128b8a9b0baa64f32001624eb12c5a
zhaoyafei <zhaoyafei0210@gmail.com>
2018-12-15 14:31:09 +0800

    add mxnet/*, tensorflow/*

---
+++ tensorflow/pip_install_tensorflow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Current release for CPU-only
+#pip install tensorflow
+
+# Nightly build for CPU-only (unstable)
+#pip install tf-nightly
+
+# GPU package for CUDA-enabled GPU cards
+pip install tensorflow-gpu
+
+# Nightly build with GPU support (unstable)
+#pip install tf-nightly-gpu
---
+++ tensorflow/pip_upgrade_tensorflow_with_eager.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pip install -q --upgrade tensorflow==1.11
